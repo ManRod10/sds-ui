df14e6ac01926285dbcb31680da8db8434b9cf6a
ManRod10 <marodriguo@eafit.edu.co>
2023-04-26 12:10:15 -0500

    Update Calls.jsx

--- src/components/modules/Calls.jsx
+++ src/components/modules/Calls.jsx
@@ -20,6 +20,15 @@ import ukri from '../../media/UKRI.svg';
 /* UI library Sub-components */
 const { Title } = Typography;
 
+function Tweet() {
+  const options = { width: 300 ,  height: 700};
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center'  }}>
+      <TwitterTweetEmbed tweetId='1638254153192591361' options={options} />
+    </div>
+  );
+}
+
 const Calls = ({ core }) => {
   useEffect(() => {
     document.title = 'Convocatorias | SALUDATA';
@@ -44,11 +53,7 @@ const Calls = ({ core }) => {
           src={logo_sds_color}
           alt="Logotipo Secretaría de Salud de Bogotá"
         />
-        <div style={{display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-        <TwitterTweetEmbed
-          tweetId={'1638254153192591361'} 
-        />
-        </div>
+        {Tweet()}
         <Divider />
         <Title className="bold" level={3}>
           NIH Grants & Funding
